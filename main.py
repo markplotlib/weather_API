932c3532c93ea0f436cf97a66de1a954bafd8a84
markplotlib <mark.chesney@gmail.com>
2021-04-28 06:30:45 -0700

    refactor: move parse_to_record into util.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import requests
 import json
 from datetime import datetime
 
-from util import to_timezone
+from util import to_timezone, parse_to_record
 
 # read in token
 parser = configparser.ConfigParser()
@@ -25,31 +25,9 @@ request = 'http://api.openweathermap.org/data/2.5/weather' \
 resp: requests.models.Response = requests.get(request)
 
 # convert text attribute to JSON format
-meta = json.loads(resp.text)
+metadata = json.loads(resp.text)
 
-
-###### parse meta dictionary
-# a.	City ID
-id = meta['id']
-
-# b.	City name
-name = meta['name']
-
-# c.	Datetime – convert from Unix timestamp to EST
-ts = meta['dt']
-utc: datetime = datetime.utcfromtimestamp(ts)
-est: datetime = to_timezone(utc, 'US/Eastern')
-
-# d.	Weather description
-desc = meta['weather'][0]['description']
-
-# e.	Current temperature
-temp_F = meta['main']['temp']
-
-# f.	Feels like temperature
-temp_feels = meta['main']['feels_like']
-######
-
-print(id, name, est, desc, temp_F, temp_feels, sep='; ')
+# parse metadata into record
+rec = parse_to_record(metadata)
 
 # store in database
--- util.py
+++ util.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import pytz
 
 def to_timezone(dt, tz_name):
@@ -19,3 +20,41 @@ def to_timezone(dt, tz_name):
     local = pytz.utc.localize(dt)
     tz_target = pytz.timezone(tz_name)
     return local.astimezone(tz_target)
+
+
+def parse_to_record(meta):
+    """
+    Parse metadata into 1 record
+
+    Parameters
+    ----------
+    meta : dict
+        metadata selected from this specific JSON object
+
+    Returns
+    -------
+    tuple
+        record of selected features
+    """
+    # a.	City ID
+    id = meta['id']
+
+    # b.	City name
+    name = meta['name']
+
+    # c.	Datetime – convert from Unix timestamp to EST
+    ts = meta['dt']
+    utc: datetime = datetime.utcfromtimestamp(ts)
+    est: datetime = to_timezone(utc, 'US/Eastern')
+    str_est = str(est)
+
+    # d.	Weather description
+    desc = meta['weather'][0]['description']
+
+    # e.	Current temperature
+    temp_F = meta['main']['temp']
+
+    # f.	Feels like temperature
+    temp_feels = meta['main']['feels_like']
+
+    return (id, name, str_est, desc, temp_F, temp_feels)
