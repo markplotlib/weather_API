011fae117b80eaa5e219fd10bf773a7d87641803
markplotlib <mark.chesney@gmail.com>
2021-04-30 08:31:53 -0700

    fix: add domestic USA cities (hot and cold), and query db

--- main.py
+++ main.py
@@ -29,10 +29,59 @@ print('Inserting city weather reports:')
 populate_table(cities)
 
 # query database to verify
-print("HOTTEST city, database query: {0.city}.".format(
+print("HOTTEST city, database query: {0.city}.\n".format(
     Report.select().order_by(Report.temp_F.desc()).get()
 ))
 
-print("COOLEST city, database query: {0.city}.".format(
+print("COOLEST city, database query: {0.city}.\n".format(
+    Report.select().order_by(Report.temp_F.asc()).get()
+))
+
+
+###########
+import requests, json
+from util import build_http_req, parse_to_record
+from database import _add_report
+
+new_request = build_http_req(city='Laredo', state='TX',
+                             country='US', units='imperial')
+
+# call HTTP get method; store response to new_request
+resp: requests.models.Response = requests.get(new_request)
+
+# convert text attribute to JSON format
+metadata = json.loads(resp.text)
+
+# parse metadata into record
+record = parse_to_record(metadata)
+
+# store record into database
+_add_report(record)
+
+#   -   -   -   -
+new_request = build_http_req(city='Fairbanks', state='AK',
+                             country='US', units='imperial')
+
+# call HTTP get method; store response to new_request
+resp: requests.models.Response = requests.get(new_request)
+
+# convert text attribute to JSON format
+metadata = json.loads(resp.text)
+
+# parse metadata into record
+record = parse_to_record(metadata)
+
+# store record into database
+_add_report(record)
+print()
+
+#   -   -   -   -
+
+# query database to verify
+print("HOTTEST city, database query: {0.city}.\n".format(
+    Report.select().order_by(Report.temp_F.desc()).get()
+))
+
+print("COOLEST city, database query: {0.city}.\n".format(
     Report.select().order_by(Report.temp_F.asc()).get()
 ))
